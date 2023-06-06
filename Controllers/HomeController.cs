44b4bb22ac2c4a4a4e564845c25912202513353e
Matias Luengo <matiasluengoo95@gmail.com>
2023-06-06 16:51:51 -0300

    First Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using OrtopediaDM.Data;
+using OrtopediaDM.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrtopediaDM.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment env;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context, IWebHostEnvironment env)
+        {
+            _logger = logger;
+            _context = context;
+            this.env = env;
+
+        }
+
+        //public IActionResult Index()
+        public async Task<IActionResult> Index()
+        {
+            var datosAmostrar = _context.Producto;
+
+            return View(await datosAmostrar.ToListAsync());
+
+            //return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public async Task<IActionResult> about_us()
+        {
+            var datosAmostrar = _context.Marca;
+            return View(await datosAmostrar.ToListAsync());
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
